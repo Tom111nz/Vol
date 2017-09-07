33f2d1ee6813c675f0f0d13774352871cc2d0f5f
Tom111nz <amandatomnz@gmail.com>
2017-09-07 21:42:21 +1200

    Working now, but only for csv files.

--- LoadVIXFutureInterestRate.py
+++ LoadVIXFutureInterestRate.py
@@ -16,14 +16,16 @@ import math
 con = mdb.connect(host="localhost",user="root",
                   passwd="password",db="Vol")
 
-cur = con.cursor()
-cur.execute("select * from vixfuturesexpiry where expirydate = '2017-08-16' order by expiryDate")
-rawExpiry = cur.fetchall()
-cur.close()
-
 thirtyDaysInMinutes = 30*24*60
 oneYearInMinutes = 365*24*60
 timeScaler = thirtyDaysInMinutes / oneYearInMinutes
+now = datetime.datetime.now()
+nowStr = now.strftime("%Y-%m-%d")
+
+cur = con.cursor()
+cur.execute("select * from VIXFuturesExpiry where impliedrate = -1 and expiryDate < " + nowStr + " order by expirydate desc")
+rawExpiry = cur.fetchall()
+cur.close()
 
 for row in rawExpiry:
     temp = row[1]
@@ -47,48 +49,42 @@ for row in rawExpiry:
         print("Number of rows: " + str(numRows))
         matrix = my_list[0:]
         rateList = list()
+        put_LetterP = matrix[2][3] # use whatever is the put designation ("P" or "Put")
+        print("put_LetterP: " + str(put_LetterP))
         for rowNew in range(2,numRows-1): # starting with row 2 means we ignore the header and first put (because is difficult to calculate delta_K)
             currentRow = matrix[rowNew]
-            print(currentRow[3])
-            letterP = "P"
-            print(currentRow[3] == letterP)
-            if currentRow[3] == letterP:
-                nextRow = matrix[rowNew+1]
-                previousRow = matrix[rowNew-1]
+            nextRow = matrix[rowNew+1]
+            previousRow = matrix[rowNew-1]
+            if currentRow[3] == put_LetterP and nextRow[3] == put_LetterP:               
                 contributionByStrike = float(currentRow[6])
                 tradePrice = float(currentRow[4])
                 strikeCalc = (0.5*(float(nextRow[2]) - float(previousRow[2])))/(math.pow(float(currentRow[2]), 2))
                 rate = 1/timeScaler * (math.log(contributionByStrike/(tradePrice*strikeCalc)))
                 rateList.append(rate)
             else:
-                print(len(rateList))
+                #print(len(rateList))
                 # calculate average, max and min, and then exit
                 if len(rateList) > 0:
-                    print("in Here")
                     maxRate = max(rateList)
                     minRate = min(rateList)
                     meanRate = sum(rateList) / len(rateList)
                     print(maxRate)
                     print(minRate)
                     print(meanRate)
-                    # don't insert if difference is greater than 0.00005 ?
+                    # don't insert if difference is greater than 0.001 ?
+                    if abs(maxRate - minRate) < 0.001:
+                        print("update VIXFuturesExpiry set impliedrate = "'"%s"'"  where contract = "'"%s"'"" % (meanRate, row[0]))
+                        cur = con.cursor()
+                        cur.execute("update VIXFuturesExpiry set impliedrate = "'"%s"'"  where contract = "'"%s"'"" % (meanRate, row[0]))
+                        con.commit()
+                        cur.close()
+                    else:
+                        print("Difference between maxRate and minRate was "'"%s"'"  for "'"%s"'""  % (str(abs(maxRate - minRate)) , str(row[0])))
+                        print("Tolerance is 0.001. No insert made to database. You need to fix this and work out why the difference between maxRate and minRate is so large")
+                        for rowI in rateList:
+                            print(rowI)
                     # put these values in the database. Add columns to VixFutureExpiry table and populate. Need to make initial SQL query dynamic (use current month?)
                     break
-                
-                
-
-##now = datetime.datetime.now()
-##
-#### We go to the website and download the data for the rest of this year and for all of next year
-##
-##thisYear = str(now.year)
-##VIXFutureList = list()
-##print(contractExpiries[now.month -1:])
-##
-##for mon in contractExpiries[now.month -1:]:
-##    aFile = 'http://cfe.cboe.com/Publish/ScheduledTask/MktData/datahouse/CFE_' + mon + thisYear[2:] + '_VX.csv'
-##    VIXFutureList.append(aFile)
-##nextYear = str(now.year + 1)
-##for mon in contractExpiries:
-##    aFile = 'http://cfe.cboe.com/Publish/ScheduledTask/MktData/datahouse/CFE_' + mon + nextYear[2:] + '_VX.csv'
-##    VIXFutureList.append(aFile)
+                else:
+                    print("length of rateList is zero for "'"%s"'"" % str(row[0]))
+                    print("No insert made to database. You need to fix this and work out why the length of rateList is zero")
