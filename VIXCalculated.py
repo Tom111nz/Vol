bb3ed28aacb995a5270da7e5d283ae7b74b2e24f
Tom111nz <amandatomnz@gmail.com>
2018-05-22 20:13:53 +1200

    Make dynamic

--- VIXCalculated.py
+++ VIXCalculated.py
@@ -11,6 +11,31 @@ con = mdb.connect(host="localhost",user="root",
 DeltaUsed = 0.7
 InterestRateUsed = 0.01
 calculateVIXFromSingleExpiry_PrintResults = False
+rowsInserted = 0
+
+
+## check we are not missing an option expiry in VIXFutureOptionExpiryList()
+sqlExpiries = ('select expiration from optionexpiry '
+            'where quote_date = (select max(quote_date) from optionexpiry) '
+            'and root = "SPX"')
+cur = con.cursor()
+cur.execute(sqlExpiries)
+expiries = cur.fetchall()
+cur.close()
+expiriesList = list()
+expiriesAlreadyInList = list()
+for item in expiries:
+    expiriesList.append(item[0])
+for item in VIXFutureOptionExpiryList():
+    expiriesAlreadyInList.append(datetime.datetime.strptime(item[2] + ' 08:30:00', "%Y-%m-%d %H:%M:%S"))
+
+outer = set(expiriesList) - set(expiriesAlreadyInList)
+if len(outer) > 0:
+    print('List of option expiries not yet in VIXFutureOptionExpiryList')
+    for item in sorted(outer):
+        print(item)
+
+
 for sheetNum, row in enumerate(VIXFutureOptionExpiryList()):
     
     FuturesContract = row[0]
@@ -20,10 +45,15 @@ for sheetNum, row in enumerate(VIXFutureOptionExpiryList()):
     optionExpiryDatetime = datetime.datetime.strptime(optionExpiryString, "%Y-%m-%d %H:%M:%S")
 
     ## break if is an old contract
-    if optionExpiryDatetime.year < 2018:
+    sqlEarliestExpiry = ('select min(expiration) from optionexpiry '
+                'where quote_date = (select max(quote_date) from optionexpiry) '
+                'and root = "SPX"')
+    cur = con.cursor()
+    cur.execute(sqlEarliestExpiry)
+    earliestExpiry = cur.fetchone()
+    cur.close()
+    if optionExpiryDatetime < earliestExpiry[0]: ## process from the earliest option expiry, and onwards       
         continue
-
-    
 ## Get days where data exists for the option expiry
     sqlQuery = ('select oe.quote_date, und.underlying_bid_1545 from OptionExpiry oe '
                 'left join underlying und on oe.id = und.optionexpiryid '
@@ -34,7 +64,7 @@ for sheetNum, row in enumerate(VIXFutureOptionExpiryList()):
     cur.execute(sqlQuery)
     quoteDatesOptionsRaw = cur.fetchall()
     cur.close()
-
+    
     for row in quoteDatesOptionsRaw:
         quoteDate = row[0]
         if datetime.datetime(quoteDate.year, quoteDate.month, quoteDate.day) <= datetime.datetime(optionExpiryDatetime.year, optionExpiryDatetime.month, optionExpiryDatetime.day):
@@ -51,5 +81,8 @@ for sheetNum, row in enumerate(VIXFutureOptionExpiryList()):
                           (parse(quoteDateKey).strftime("%Y-%m-%d %H:%M:%S"), FuturesContract, optionExpiryString, InterestRateUsed, DeltaUsed, VIXCalculated))
                 cur.close()
                 con.commit()
+                print(str(optionExpiryDatetime))
+                rowsInserted = rowsInserted + 1
             else:
                 cur.close()
+print('VIXCalculated inserted %s rows' % str(rowsInserted)) 
